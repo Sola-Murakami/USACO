a7cec982f1083d92d9521821d0dd0c7d9deae5f7
Sola-Murakami <87738645+Sola-Murakami@users.noreply.github.com>
2021-07-21 15:36:57 -1000

    Update ride.py

--- 1.2/ride.py
+++ 1.2/ride.py
@@ -3,30 +3,24 @@ ID: solasky1
 LANG: PYTHON3
 TASK: ride
 """
-
-fin = open('ride.in', 'r')
-fout = open('ride.out', 'w')
-
 def let_to_num(let):
     return ord(let) - 64
 
 def go_or_stay(boo):
-    if boo == True:
-        return 'GO'
-    else:
-        return 'STAY'
+    return 'GO' if boo else 'STAY'
 
-s = set()
+with open('ride.in') as f:
+    s = set()
+    
+    for _ in range(2):
+        st = f.readline().strip()
+        num = 1
 
-for _ in range(2):
-    st = fin.readline()
-    num = 1
+        for let in st:
+            num *= let_to_num(let)
+            num %= 47
 
-    for let in st:
-        num *= let_to_num(let)
-        num %= 47
+        s.add(num)
 
-    s.add(num)
-
-fout.write(go_or_stay(len(s) == 1) + '\n')
-fout.close()
+with open('ride.out', 'w') as f:
+    f.write(f'{go_or_stay(len(s) == 1)}\n')
