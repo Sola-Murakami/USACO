752ca4b3d24d3f94e132bd346d799964531350b9
Sola Murakami <87738645+Sola-Murakami@users.noreply.github.com>
2021-07-23 17:19:58 -1000

    Update fracdec.py

--- 2.4/fracdec.py
+++ 2.4/fracdec.py
@@ -1,57 +1,40 @@
 """
-ID: solasky1
+ID: 
 LANG: PYTHON3
 TASK: fracdec
 """
+def frac_to_dec(numer, denom):
+    times = ''
+    if numer < 0 and denom > 0 or numer > 0 and denom < 0:
+        times = '-'
+        numer = abs(numer)
+        denom = abs(denom)
+    if numer < 0 and denom < 0:
+        numer *= -1
+        denom *= -1
+    
+    dec = []
+    rems = []
+    
+    while True:
+        dec.append(str(numer // denom))
+        numer %= denom
+        rems.append(numer)
+        numer *= 10
+        
+        if numer == 0:
+            if len(dec) == 1:
+                return times + dec[0] + '.0'
+            return times + dec[0] + '.' + ''.join(dec[1:])
+        
+        if rems.count(rems[-1]) > 1:
+            dec.insert(rems.index(rems[-1]) + 1, '(')
+            dec.append(')')
+            return times + dec[0] + '.' + ''.join(dec[1:])
 
+with open('fracdec.in') as f:
+    numer, denom = map(int, f.readline().strip().split())
 
-def input():
-    with open("fracdec.in", "r") as f:
-        return tuple(map(int, f.readline().strip().split()))
-
-
-def divide(n, d):
-    return (n // d, n % d)
-
-
-def decimal(n, d):
-    whole, n = divide(n, d)
-    decimals = []
-    used = set()
-
-    q, r = divide(n * 10, d)
-    while (q, r) not in used:
-        # no repeating
-        if (q, r) == (0, 0):
-            break
-
-        used.add((q, r))
-        decimals.append((q, r))
-        q, r = divide(r * 10, d)
-
-    repeat_idx = decimals.index((q, r)) if (q, r) != (0, 0) else len(decimals)
-
-    return (
-        whole,
-        [d[0] for d in decimals[:repeat_idx]],
-        [d[0] for d in decimals[repeat_idx:]]
-    )
-
-
-def output(whole, non_repeating, repeating):
-    res = str(whole) + "."
-    if non_repeating or repeating:
-        res += "".join(map(str, non_repeating))
-        if repeating:
-            res += "(%s)" % "".join(map(str, repeating))
-    else:
-        res += "0"
-
-    with open("fracdec.out", "w") as f:
-        for line in [res[i:i+76] for i in range(0, len(res), 76)]:
-            f.write(line + "\n")
-
-
-N, D = input()
-res = decimal(N, D)
-output(*res)
+with open('fracdec.out', 'w') as f:
+    dec = frac_to_dec(numer, denom)
+    f.write(f'{dec}\n')
